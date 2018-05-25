f570ac8a7be8bbf1042873799331ad24f99398f0
BigBigBigWhale <15755028927@163.com>
2018-05-26 02:09:10 +0800

    登陆界面的样式

--- src/module/login.vue
+++ src/module/login.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="login">
-    登陆
+    <div class="login-title">
+      登录
+      <div class="language">
+        <select name="" id="">
+          <option value="">简体中文</option>
+          <option value="">English</option>
+        </select>
+      </div>
+    </div>
+    <div class="login-sign">
+      <img src="../assets/login_title.png" alt="">
+    </div>
+    <form>
+      <div class="login-input-box">
+        <input type="text" name="user" placeholder="手机号">
+      </div>
+      <div class="login-input-box">
+        <input type="password" name="password" placeholder="密码">
+      </div>
+      <button type="submit">登录</button>
+      <div class="login-tips">
+        <span>注册账号</span><span>忘记密码</span>
+      </div>
+    </form>
   </div>
 </template>
 

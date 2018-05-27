d38b25c4577ca2b2c21fbca2772776bc5fcb3898
BigBigBigWhale <15755028927@163.com>
2018-05-27 21:50:19 +0800

    优化登陆状态下，跳到登录页面的处理，优化底部导航对于页面地址变换的更改

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@
 
     <footer class="footer" v-if="hasFooter">
         <ul>
-          <li :class="footerOpt=='wallet'?'active':''" @click="checkFooterOpt('wallet')"><BaseButton><router-link to="/"><img src="./assets/icon/wallet.svg" alt="钱包">钱包</router-link></BaseButton></li>
-          <li :class="footerOpt=='price'?'active':''" @click="checkFooterOpt('price')"><BaseButton><router-link to="/price"><img src="./assets/icon/price.svg" alt="行情">行情</router-link></BaseButton></li>
-          <li :class="footerOpt=='trade'?'active':''" @click="checkFooterOpt('trade')"><BaseButton><router-link to="/trade"><img src="./assets/icon/trade.svg" alt="交易">交易</router-link></BaseButton></li>
-          <li :class="footerOpt=='personal'?'active':''" @click="checkFooterOpt('personal')"><BaseButton><router-link to="/personal"><img src="./assets/icon/personal.svg" alt="我的">我的</router-link></BaseButton></li>
+          <li :class="footerOpt=='/'?'active':''" @click="choiceFooterOpt('/')"><BaseButton><router-link to="/"><img src="./assets/icon/wallet.svg" alt="钱包">钱包</router-link></BaseButton></li>
+          <li :class="footerOpt=='/price'?'active':''" @click="choiceFooterOpt('/price')"><BaseButton><router-link to="/price"><img src="./assets/icon/price.svg" alt="行情">行情</router-link></BaseButton></li>
+          <li :class="footerOpt=='/trade'?'active':''" @click="choiceFooterOpt('/trade')"><BaseButton><router-link to="/trade"><img src="./assets/icon/trade.svg" alt="交易">交易</router-link></BaseButton></li>
+          <li :class="footerOpt=='/personal'?'active':''" @click="choiceFooterOpt('/personal')"><BaseButton><router-link to="/personal"><img src="./assets/icon/personal.svg" alt="我的">我的</router-link></BaseButton></li>
         </ul>
     </footer>
   </div>
@@ -44,23 +44,35 @@ export default {
     '$route': function(to,from){//监控页面地址是否需要头部、底部导航栏和登陆状态的实时检测
       this.hasFooter = !!hasFooter[to.path];
       this.hasNav = !!hasNav[to.path];
-      this.checkLogin(to.path);
+      this.checkLogin(to.path,from.path);
+      this.checkStatus(to.path);
 //            console.log('val1',to);
 //            console.log('val2',from);
     }
   },
   methods:{
-    checkLogin (path){//检查登陆状态
-      if(!!!unwantedLogin[path]&&!globalDate.user){//未登录的跳转到登陆页面
-        this.$router.push("/login");//暂时隐藏，等登录做好了
+    checkLogin (toPath,FromPath){//检查登陆状态
+      if(!!!unwantedLogin[toPath]&&!globalDate.user){//未登录的跳转到登陆页面
+        this.$router.push("/login");
         return;
       }
-      if(!!unwantedLogin[path]&&globalDate.user){//登录的跳转到首页
-        this.$router.push("/");
-        return;
+      if(!!unwantedLogin[toPath]&&globalDate.user){//登录的跳转到首页
+        if(FromPath){//用于判断从哪个页面来的
+          this.$router.push(FromPath);
+        }else{
+          this.$router.push("/");
+          return;
+        }
       }
     },
-    checkFooterOpt (opt){//底部导航的选择事件
+    checkStatus (path){
+      this.hasFooter = !!hasFooter[path];
+      this.hasNav = !!hasNav[path];
+      if(this.hasFooter){
+          this.choiceFooterOpt(path);
+      }
+    },
+    choiceFooterOpt (opt){//底部导航的选择事件
       this.footerOpt = opt;
     },
     backBtn (){//头部导航的返回按钮
@@ -74,8 +86,7 @@ export default {
     let storage = new Storage('login', window.localStorage);
     globalDate.user = storage.get('user') || null;
     this.checkLogin(this.$route.path);
-    this.hasFooter = !!hasFooter[this.$route.path];
-    this.hasNav = !!hasNav[this.$route.path];
+    this.checkStatus(this.$route.path);
   },
   mounted (){
       console.log(this.$route.path)

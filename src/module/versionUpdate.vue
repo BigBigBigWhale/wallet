49314a5d293a240592d9df2e26c3cd4804e0a2a6
yanshaoxi <1421441479@qq.com>
2018-06-10 23:33:02 +0800

    E:\wallet

--- src/module/versionUpdate.vue
+++ src/module/versionUpdate.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="trade cusService">
+  <div class="trade update">
     <div class="trade-nav">
       <span class="trade-nav-left"><img src="/static/images/icon/icon-back.png" alt=""/>返回</span>
-      客服
-      <span class="trade-nav-right">反馈记录</span>
+      版本更新
     </div>
-    <div class="info">您有任何意见或者建议都可以反馈给我们。客服将在24小时内回复</div>
-    <textarea class="serviceTxt" name="" id=""  rows="10"></textarea>
-    <a class="submit" href="#">提交</a>
+    <div class="isUpdate">当前已是最新版本</div>
+    <div class="notUpdate">
+      <p>检测到更新的版本</p>
+      <p>建议更新</p>
+      <a class="submit" href="#">立即更新</a>
+    </div>
+
   </div>
 </template>
 
 <script>
-    import '../style/cusService.less'
+    import '../style/versionUpdate.less'
 </script>
